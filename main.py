a1a96576b12a7c6ae44b6f5e5021b20c0275ad6b
Kevin Lin <lin.kevin.1923@gmail.com>
2025-07-11 11:26:04 +0000

    Ignore virtual environment and .env file

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"Hello": "Kevin Live Test"}
+async def read_root():
+    async with async_session() as session:
+        result = await session.execute(text("SELECT 'Hello Kevin from DB!'"))
+        return {"message": result.scalar()}
 
